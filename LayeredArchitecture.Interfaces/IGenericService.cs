4900ccd4243bd149a782128ccde1a23adaeb3210
alperclk <160202023@kocaeli.edu.tr>
2020-07-31 21:50:05 +0300

    Add files via upload

---
+++ LayeredArchitecture.Interfaces/IGenericService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredArchitecture.Interfaces
+{
+    public interface IGenericService<T>:IDisposable
+    {
+        T Add(T entity);
+
+        T Get(int id);
+
+        List<T> GetAll();
+
+        List<T> GetAll(Expression<Func<T, bool>> predicate);
+
+        bool Remove(T entity);
+
+        bool Remove(int id);
+
+        T Update(T entity);
+
+
+    }
+}
