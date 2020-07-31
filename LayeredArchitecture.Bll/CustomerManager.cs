c330428a00f39b7e67ad2045174e100fdf3c4679
alperclk <160202023@kocaeli.edu.tr>
2020-07-31 21:53:19 +0300

    Add files via upload

---
+++ LayeredArchitecture.Bll/CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using LayeredArchitecture.Dal.Abstract;
+using LayeredArchitecture.Entities.Models;
+using LayeredArchitecture.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace LayeredArchitecture.Bll
+{
+    public class CustomerManager:GenericManager<Customers>,ICustomerService
+    {
+        ICustomerRepository customerRepository;
+
+        public CustomerManager(ICustomerRepository customerRepository):base(customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public List<Customers> CustomerListByCity(string city)
+        {
+            return customerRepository.CustomersList(city);
+        }
+    }
+}
---
+++ LayeredArchitecture.Bll/GenericManager.cs
@@ -0,0 +1,71 @@
+﻿using LayeredArchitecture.Dal.Abstract;
+using LayeredArchitecture.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredArchitecture.Bll
+{
+    public abstract class GenericManager<T> : IGenericService<T> where T:class
+    {
+
+        private readonly IGenericRepository<T> genericRepository;
+
+        protected GenericManager(IGenericRepository<T> genericRepository)
+        {
+            this.genericRepository = genericRepository;
+        }
+
+        public T Add(T entity)
+        {
+            return genericRepository.Add(entity);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                genericRepository.Dispose();
+            }
+        }
+
+        public T Get(int id)
+        {
+            return genericRepository.Get(id);
+        }
+
+        public List<T> GetAll()
+        {
+            return genericRepository.GetAll();
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> predicate)
+        {
+            return genericRepository.GetAll(predicate);
+        }
+
+        public bool Remove(T entity)
+        {
+            return genericRepository.Remove(entity);
+        }
+
+        public bool Remove(int id)
+        {
+            return genericRepository.Remove(id);
+        }
+
+        public T Update(T entity)
+        {
+            return genericRepository.Update(entity);
+        }
+    }
+}
